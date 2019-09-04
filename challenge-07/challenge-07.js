7b7331fbf7313bb4adf0f577bb5d522817a1ef27
Matheus Felipe <36232629+itheu@users.noreply.github.com>
2019-09-04 13:27:28 -0300

    Update challenge-07.js

--- challenge-07/challenge-07.js
+++ challenge-07/challenge-07.js
@@ -1,39 +1,38 @@
 /*
 Crie um array com 5 items (tipos variados).
 */
-// ?
-
+var array = ['Matheus', 13, { carro: 'GOL' }, function teste(){return array;}, [1,2,3]];
 /*
 Crie uma função chamada `addItem`, que irá adicionar itens no array criado.
 A função deverá retornar o array atualizado.
 */
-// ?
+function addItem(item){
+    array.push(item)
+    return array
+}
 
 /*
 Adicione um novo array ao array criado no início do desafio, com ao menos 3
 itens de tipos diferentes, mostrando o resultado no console.
 */
-// ?
+addItem([13, 'é', 'GALO']);
 
 /*
 Mostre no console o segundo elemento desse último array, criado acima, com a
 frase:
 "O segundo elemento do segundo array é [ELEMENTO]."
 */
-// ?
-
+console.log('O segundo elemento do segundo array é: ' + array[5][2])
 /*
 Mostre no console quantos itens tem o primeiro array criado, com a frase:
 "O primeiro array tem [QUANTIDADE DE ITENS] itens."
 */
-// ?
-
+console.log('O primeiro array tem ' + array.length + 'itens')
 /*
 Agora mostre no console quantos itens tem o segundo array criado, com a frase:
 "O segundo array tem [QUANTIDADE DE ITENS] itens."
 */
-// ?
-
+console.log('O segundo array tem ' + array[5].length + ' itens.')
 /*
 Utilizando a estrutura de repetição `while`, mostre no console todos os números
 pares entre 10 e 20, inclusive esses 2.
